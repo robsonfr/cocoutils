f90b861916dd5e2416b99facf5e0baba4e57ffa1
robsonsfranca <robsonsfranca@d1f9147c-39cb-11de-a9fe-ad1e22fc3a8a>
2010-01-16 12:41:55 +0000

    - PAK file support added TODO: CAS file support

--- trunk/code/src/cocoutils/main.py
+++ trunk/code/src/cocoutils/main.py
@@ -4,7 +4,14 @@ Created on 15/01/2010
 @author: Robson
 '''
 
-import cocoutils
+from cocoutils.pak.pakfile import * 
 
 if __name__ == '__main__':
-    print cocoutils.__doc__
+    pakf = PAKHeader("d:/king.pak")
+    print "%X %X" % (pakf.length, pakf.baseAddress)
+    pakf.savefile("d:/k2.pak")
+    pak2 = PAKHeader()
+    pak2.build(0x4CFF,"AAAAA","BBBBB")
+    pak2.savefile("d:/k3.pak")
+    
+    
